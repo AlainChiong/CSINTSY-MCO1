4024071369a417c45de60b4ed3c5a744661b91ca
AlainChiong <alain.chiong2004@gmail.com>
2025-06-24 07:32:07 +0800

    placed parameters on searches, added a way for new node input from user, made interface

--- Main.java
+++ Main.java
@@ -2,18 +2,34 @@ import java.util.*;
 
 public class Main{
 
-    public static void BFS(){
+    public static void BFS(String start, String goal, Node[] nodeList){
         // This gonna be for bfs, input parameters still not final
         // Queue<Integer> q = new LinkedList<>();
     }
 
-    public static void A_Star(){
+    public static void A_Star(String start, String goal, Node[] nodeList){
         // This gonna be for A*, input parameters still not final
         // Queue<Integer> q = new LinkedList<>();
     }
 
-    public static void Search(){
+    public static void Search(Scanner sc, Node[] nodeList){
+        System.out.print("Enter start node:");
+        String start = sc.nextLine();
+        System.out.print("Enter goal node:");
+        String goal = sc.nextLine();
+        System.out.println("1. BFS (Blind Search)\n2. A* (Heuristic Search)");
+        int input = sc.nextInt();
 
+        switch(input){
+            case 1:
+                BFS(start, goal, nodeList);
+                break;
+            case 2:
+                A_Star(start, goal, nodeList);
+                break;
+            default:
+                break;
+        }
     }
     
     public static void main(String args[]){
@@ -23,10 +39,10 @@ public class Main{
             arr[i] = i + 1;
         }
         
-        Node[] nodeList = new Node[21];
+        Node[] nodeList = new Node[100];
         int nNode;// incase we need to add a new node
         for (nNode = 0; nNode < 21; nNode++){
-            nodeList[nNode] = new Node((String)('A' + nNode));
+            nodeList[nNode] = new Node(String.valueOf((char)('A' + nNode)));
         }
         System.out.println(nNode);
         boolean isok = nodeList[0].addEdge(nodeList[1]); // check if adding edge is successful
@@ -37,19 +53,31 @@ public class Main{
         //interface
         boolean running = true;
         while(running){
-            System.out.println("Available locations");
+            System.out.println("\nAvailable locations");
             for (int i = 0; i < nNode; i++){
-                System.out.print(nodeList[i].getName()+", ");
+                System.out.print(nodeList[i].getName());
+                if(i != nNode - 1){
+                    System.out.print(", ");
+                }
             }
-            System.out.println("1. Input start and goal\n2. Input new node\n3. Exit");
+            System.out.println("\n1. Input start and goal\n2. Input new node\n3. Exit");
             int choice = sc.nextInt();
             sc.nextLine();
 
             switch(choice){
-                case '1':
-                    Search();
+                case 1:
+                    Search(sc, nodeList);
                     break;
-                case '2':
+                case 2:
+                    System.out.print("Enter node name:");
+                    String nName = sc.nextLine();
+                    nodeList[nNode] = new Node(nName);
+                    nNode++;
+                    // edge part here
+                    break;
+                case 3:
+                    System.out.print("Goodbye");
+                    running = false;
                     break;
                 default:
                     System.out.println("Invalid option");
